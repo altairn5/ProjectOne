1b6c211e44cbf41d3bfe3f34a671b6360aa26572
Humberto Calero <altairn5@uga.edu>
2016-02-22 18:06:46 -0800

    Server.js file cleaned up

--- server.js
+++ server.js
@@ -1,138 +1,162 @@
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    cookieParser = require("cookie-parser"),
-    db = require('./models'),
-    session = require('express-session'),
-    path = require('path'),
-    bcrypt = require('bcrypt'),
-    keygen = require('keygenerator'),
-    methodOverride = require('method-override'),
-    app = express();
 
+/*Require dependecies*/
+var express = require('express'),
+bodyParser = require('body-parser'),
+cookieParser = require("cookie-parser"),
+db = require('./models'),
+session = require('express-session'),
+path = require('path'),
+bcrypt = require('bcrypt'),
+keygen = require('keygenerator'),
+methodOverride = require('method-override'),
+app = express();
+
+
+//path.join("projectOne/", "/public");
 var views = path.join(process.cwd(), "/views");
-            // or we can write 
-            //path.join("projectOne/", "/public");
- // CONFIG //
-// serve js & css files
+
+/*CONFIG 
+.js & .css files + vendor files*/
 app.use("/static", express.static("public"));
 app.use("/vendor", express.static("bower_components"));           
 
-//APPs
-app.use(bodyParser.urlencoded({extended: true})); // parse POSTed data
-app.use(cookieParser("Super Secret")); // parse cookie data
+//Parse serialized data being posted
+app.use(bodyParser.urlencoded({extended: true})); 
+// parse cookie data
+app.use(cookieParser("Super Secret")); 
 
-    //create The Session
-app.use(session({
-    //use keygen to generate a secret key
-    secret: keygen._({specials: true}),
-    resave: false,
-    saveUninitialized: true
-
-})
+//Session Setup Process
+app.use(session({ secret: keygen._({specials: true}),
+                  resave: false,
+                  saveUninitialized: true
+                })
 );
-    //extending the request object to help manange sessions
-app.use(function (req,res,next){
-    //login a user
-    req.login = function (user) {
-      console.log(user._id);
-        req.session.userId = user._id;
-    };
-    //find the current user
-    req.currentUser = function (cb){
-        db.User.findOne({_id: req.session.userId}, function (err, user) {
-                    req.user = user;
 
-                    cb(null, user);
-                }
-            )
+
+
+/* extending the request object to help manange sessions*/
+app.use(function (req,res,next){
+    
+    //Login user method
+    req.login = function (user) {
+        
+        console.log(user._id);
+        req.session.userId = user._id;
+    //End login     
     };
-    //logout the current user
+
+    // currentUser method
+    req.currentUser = function (cb){
+
+        //Looks for user in the db
+        db.User.findOne({_id: req.session.userId}, function (err, user) {
+            if(err) 
+            {
+                cb(err, null)
+            }
+
+            req.user = user;
+            cb(null, user);
+        })
+    //End currentUser
+    };
+
+// logout the current user
     req.logout = function () {  
+        
         req.session.userId = null;
         req.user = null;
-     }
-     next();
-});
+    }
+    //Next is needed to continue
+    next();
     
+});
 
-//VIEWS
+
+/*View Routes*/
 
 //Home View
 app.get("/home",function (req,res){
 
-if(req.currentUser){
-res.sendFile(path.join(views, "profile.html"));
-}
-res.sendFile(path.join(views, "login.html"))
+    // Uses currentUser Method to check if user is already signedIn
+    if(req.currentUser)
+    {
+        res.sendFile(path.join(views, "profile.html"));
+    }
 
+    // If new user session send login form
+    res.sendFile(path.join(views, "login.html"))
+
+//End Home
 });
 
 
+// SignUp Form Route
 app.get("/signup",function (req,res){
 
-res.sendFile(path.join(views, "signup.html"));
+    res.sendFile(path.join(views, "signup.html"));
 
+//End
 });
 
+
+//Loging Form Route
 app.get("/login",function (req,res){
 
-res.sendFile(path.join(views, "login.html"));
+    res.sendFile(path.join(views, "login.html"));
 
+//End
 });
 
-// app.get("/results", function (req, res){
-// res.sendFile(path.join(views, "results.html"));
-// });
-
-// within the profile page
-// FIND USER IN DB AND SHOW SCOREBAORDS
-// VIA RES.SEND(SCOREBOARDDATA);
-// app.get("/profile",function (req,res){
-// // shows a specifi user
-// // allow user to add a new scoreboard
-// // allow user to view all of their saved scoreboards
-// // allow user ot remove a scoreboard
-// // allow user to update data in scorebaord
-// res.sendFile(path.join(views, "profile.html"));
-
-// });
-
-// APP.POST FOR EACH Page
 
 /*User Routes*/
 
-//SignUp
+//Create New User Route
 app.post("/signup", function createUser(req, res){
-  var username = req.body.username;
-  var password = req.body.password;
+    
+    var username = req.body.username;
+    var password = req.body.password;
 
-  db.User.createSecure(username, password, function(err, user){
-    console.log("here comes the user create", user);
-    if ( user ) {
-        req.login(user);
-      // res.cookie("guid", user._id, { signed: true });
-      res.redirect("/profile")
-    } else {
-      res.redirect("/login");
-    }
-  })
+   /* Call createSecure method inside user's model file which take 2 arg & 1 cb fs 
+    and execute cb fs*/
+    db.User.createSecure(username, password, function(err, user){
+        
+       /* Once user has been created and send back we use the req.login 
+        method to create a new session*/
+        if ( user )
+        {
+            req.login(user);
+            res.cookie("guid", user._id, { signed: true });
+            res.redirect("/profile")
+        }
+        else 
+        {
+            res.redirect("/login");
+        }
 
+    //End of createSecure    
+    })
+// End createUser
 });
 
 
-//Login
+// New Session Route
 app.post("/login", function newSession(req,res){
+    
     var username = req.body.username;
-    console.log("this is the username coming", username);
     var password = req.body.password;
-    console.log("this is the password coming", password);
 
+    // Call authenticate method inside user's model file which take 2 arg & 1 cb fs
     db.User.authenticate(username, password, function (err, user){
-        if  (user){
+       
+        if(user)
+        {
             req.login(user);
-            // res.cookie("guid", user._id, { signed: true });
+            res.cookie("guid", user._id, { signed: true });
             res.redirect("/profile")
-        }else{
+        }
+        else
+        {
             console.log(err);
             res.redirect("/login");
         }
@@ -140,40 +164,55 @@ app.post("/login", function newSession(req,res){
     })
 });
 
-//End session
 
+//End Session Route
 app.get("/logout", function endSession (req,res){
-    console.log("loggingout here")
+
+    // Call req.logout method above
     req.logout;
     res.redirect("/login");
 });
 
-//Show Profile
 
+//Show Profile Route
 app.get("/profile", function showProfile (req, res){
+   
     // var username = req.body.username;
     req.currentUser(function (err, user) {
-    if (user === null) {
-      res.redirect("/login")
-    } else {
-      res.sendFile(path.join(views, "profile.html"));
-    }
-  })
+    
+        if (user === null)
+        {
+            res.redirect("/login")
+        }
+        else
+        {
+            res.sendFile(path.join(views, "show.html"));
+        }
+    })
 });
-//Show Username
+
+
+//Show Username Route Not used yet
 app.get("/username", function provideUsername (req, res){
+    
+
     req.currentUser(function (err, user){
-      if (user){
-        res.send(user);
-      }
+    
+        if (user)
+        {
+            res.send(user);
+        }
+        res.redirect('/login');
     });
-
-
+//End 
 });
+
 //Delete session
-app.delete("/logout", function(req, res) {
-  req.logout();
-  res.redirect("/login");
+app.delete("/logout", function (req, res){
+    
+    console.log("hit the correct route");
+    req.logout;
+    res.redirect("/login");
 });
 
 //City Routes
@@ -189,6 +228,6 @@ app.delete("/logout", function(req, res) {
 //       }
 //   });
 // });
-    var listener = app.listen(3000, function () {
-  console.log("Listening on port " + listener.address().port);
+var listener = app.listen(3000, function () {
+console.log("Listening on port " + listener.address().port);
 });
