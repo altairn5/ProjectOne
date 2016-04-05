26d651923ec59064a3b8d376ca38ebccb840f983
Humberto Calero <altairn5@uga.edu>
2016-04-04 19:19:54 -0700

    cleaning code

--- public/app.js
+++ public/app.js
@@ -4,83 +4,80 @@
     pageLoad();
   });
 
-  function pageLoad() {
+    function pageLoad() {
 
-           /* 1)Username Display Call
-            2)Event Listeners
-            On Prile Load Get Username For Profile Page*/
+             /* 1)Username Display Call
+              2)Event Listeners
+              On Prile Load Get Username For Profile Page*/
 
 
-            $.get("/username", function(res){
+              $.get("/username", function(res){
+                      
+                      var username = res.username
+
+                      var user = username.slice(0,1).toUpperCase()+username.slice(1).toLowerCase();
+                      $("#welcome").append("<p>Welcome, "+user+"</p>");
+                      
+                      // Call City Fav fs
+                      displayFavorites(res.city);
+
+              });
+
+
+              //New City Weather Event
+              $("form").on("submit", function(e){
                     
-                    var username = res.username
+                    e.preventDefault();
 
-                    var user = username.slice(0,1).toUpperCase()+username.slice(1).toLowerCase();
-                    $("#welcome").append("<p>Welcome, "+user+"</p>");
-                    
-                    // Call City Fav fs
-                    displayFavorites(res.city);
-
-            });
+                    var cityName = $(this).serialize();
+                    $(this).unbind(e);
+                    getCityWeather(cityName);
 
 
-            //New City Weather Event
-            $("form").on("submit", function(e){
-                  
-                  e.preventDefault();
-
-                  var cityName = $(this).serialize();
-                  $(this).unbind(e);
-                  getCityWeather(cityName);
+              });
 
 
-            });
+              // Dinamically Added #SaveCity Button needs triggers addCityToFavs fs
+              $(document).on('click','#saveCity', function (){
+
+                    addCityToFavs();
+
+              })
+
+    // End Pageload Fs
+    };
 
 
-            // Dinamically Added #SaveCity Button needs triggers addCityToFavs fs
-            $(document).on('click','#saveCity', function (){
+    // Function Get Weather For Submmitted City Name
+    function getCityWeather (city){
+  
+        // Clear Value in Search Field
+        $("#search").val(" ");
 
-                  addCityToFavs();
+        /*THERE ALSO WAYS TO NOT USE SERIALIZE()
+         Success Functions Below*/
+        $.get("/city", city, function(res){
 
-            })
+            var city = res.name;
+           
+            var temp = Math.floor((res.main.temp - 273.15) * 1.8 + 32);
+         
+            var sky = res.weather[0].main;
+            $('#div-city').empty();
 
-        // End Pageload Fs
-        };
+            var renderCity = `<p id="cityRender">${city}</p><p>${temp}`+'°F'+`</p><p>${sky}</p></p>`;
+
+            var saveCityButton = "<button class='btn btn-default saveCity' id='saveCity'>Save City</button>";
+
+            $('#div-city').append(renderCity+saveCityButton);
+
+        });
+    // End getCityWeather fs
+    }
 
 
 
 
-// Function Get Weather For Submmitted City Name
-  function getCityWeather (city){
-    
-
-    
-    $("#search").val(" ");
-
-    console.log("the city name is: " + city);
-
-  // tHERE ALSO WAYS TO NOT USE SERIALIZE()
-  $.get("/city", city, function(res){
-    
-    console.log(res);
-    var city = res.name;
-    console.log(city);
-    var temp = Math.floor((res.main.temp - 273.15) * 1.8 + 32);
-    console.log(temp);
-    var sky = res.weather[0].main;
-    $('#div-city').empty();
-
-    // $('#div-city').append('<p><h1>'+city+'</p>'+'<p>'+temp+'°F</p><p>'+sky+'</p></h1></p>');
-
-    var renderCity = `<p id="cityRender">${city}</p><p>${temp}`+'°F'+`</p><p>${sky}</p></p>`;
-    
-    var saveCityButton = "<button class='btn btn-default saveCity' id='saveCity'>Save City</button>";
-
-    $('#div-city').append(renderCity+saveCityButton);
-
-  });
-
-}
 
 function addCityToFavs(){
 
