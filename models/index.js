ce8b4b91a749fdb92066468f2f4ef82aaf2573a5
Humberto Calero <altairn5@uga.edu>
2016-04-07 17:02:13 -0700

    env variable set up

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://altairn5:colombia1999@ds019470.mlab.com:19470/heroku_f5663tt4"||"mongodb://localhost/projectOne3");
+mongoose.connect(process.env.MONGOLAB_URI||"mongodb://localhost/projectOne3");
 
 // mongoose.connect(process.env.MONGOLAB_URI ||
 //     process.env.MONGOHQ_URL || 'mongodb://localhost/projectOne3');
+
 var City = require('./city');
 var User = require('./user');
 
