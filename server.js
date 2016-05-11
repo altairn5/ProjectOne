444ab408156b9fc73e18b7a019826c8c661fe509
Humberto Calero <altairn5@uga.edu>
2016-05-10 18:29:03 -0700

    cleaning code

--- server.js
+++ server.js
@@ -308,12 +308,6 @@ app.post('/city', function (req, res) {
 
 
 
-// var listener = app.listen(3000, function () {
-// console.log("Listening on port " + listener.address().port);
-// });
-
-// var port = Number(process.env.PORT || 3000);
-
 app.listen(app.get('port'), function(){
     console.log("small weather app server listening at port 5000");
 });
